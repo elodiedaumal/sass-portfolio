f0e64156d55d27eb72ce831eb146cca5b57531ef
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-04-14 15:18:02 +0200

    created layout and react router setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Blog from './pages/Blog';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='blog' element={<Blog />} />
+        <Route path='portfolio' element={<Portfolio />} />
+        <Route path='resume' element={<Resume />} />
+        <Route path='*' element={<p>There's nothing here: 404!</p>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
